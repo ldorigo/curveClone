a071b2b24a256ee038bd9e84920346718d872f3f
Luca Dorigo <dorigoluca@gmail.com>
2016-02-20 22:21:38 +0100

    Modified title + Added holes! (and some pretty colors)

--- curveClone/core.js
+++ curveClone/core.js
@@ -1,15 +1,25 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-// position ball
+//  ball
 
-var ballX = Math.floor(Math.random() * canvas.width);
-var ballY = Math.floor(Math.random() * canvas.height);
+var ballX = Math.floor(50+Math.random() * canvas.width-100);
+var ballY = Math.floor(50+Math.random() * canvas.height-100);
 var ballRadius = 3;
 var direction = Math.floor(Math.random() * (Math.PI * 2));
 var dx = 1;
 var dy = 1;
 var speed = 2;
+var color="rgba(221,12,132,1)";
+// holes
+
+var hole=false;
+var holeStart = 0;
+var oldX=0;
+var oldY=0;
+var frameCount =0;
+var framesToHole = Math.floor(15 + Math.random()*100);
+
 
 // Keyboard
 
@@ -31,12 +41,11 @@ function radToCoordinates(rad) {    //convert a direction in radians to dx and d
         dy: dy
     };
 }
-
 function drawBall() {
     "use strict";
     ctx.beginPath();
     ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2, true);
-    ctx.fillStyle = 'blue';
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
 }
@@ -55,21 +64,23 @@ function moveBall() {
 
     dx = dx * speed / dxdy;
     dy = dy * speed / dxdy;
-
+    oldX=ballX;
+    oldY=ballY;
     ballX += dx;
     ballY += dy;
-    tempTrail.push({                //add current position to temporary trail
-        X: ballX,
-        Y: ballY,
-        radius: ballRadius
-    });
+    if(hole == false) {
+        tempTrail.push({                //if it's not in hole state, add current position to temporary trail
+            X: ballX,
+            Y: ballY,
+            radius: ballRadius
+        });
+
     counter += 1;
     if (counter > 5) {              //after a while, start copying the temporary trail to the trail (this is to avoid that the ball collides with the just-made trail)
         trail.push(tempTrail[0]);
         tempTrail = [];
         counter = 0;
-
-    }
+    }}
 }
 
 function touchWalls() {
@@ -80,12 +91,12 @@ function touchWalls() {
     return false;
 }
 
-    function distance(x1, x2, y1, y2) {  // Compute distance between 2 points
+function distance(x1, x2, y1, y2) {  // Compute distance between 2 points
     "use strict";
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
 
-    function touchTrail() {      //detect contact with trail
+function touchTrail() {      //detect contact with trail
     "use strict";
     var t, dist;
     for (t = 0; t < trail.length; t += 1) {
@@ -99,12 +110,32 @@ function touchWalls() {
     return false;
 }
 
+
+
 // main function
 
 function draw() {
     "use strict";
-    drawBall();
+    frameCount ++;
+    if(framesToHole==0){
+        hole=true;
+    }
     moveBall();
+    drawBall();
+    if(hole){
+        holeStart ++;
+        color="rgba(0,0,0,0.1)";
+        if(holeStart == 15){
+            hole = false;
+            holeStart=0;
+            framesToHole = Math.floor(15 + Math.random()*100);
+            color="rgba(221,12,132,1)";
+        }
+    }
+    else{
+        framesToHole--;
+    }
+
     if (touchWalls() || touchTrail()) {
         console.log(trail);
         alert("Game Over!");
