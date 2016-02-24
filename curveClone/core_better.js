23581bf31ca18578dca86190413f9ed4b8ec6dbe
Luca Dorigo <dorigoluca@gmail.com>
2016-02-24 20:51:47 +0100

    Added holes, and some small bugfxes.

--- curveClone/core_better.js
+++ curveClone/core_better.js
@@ -10,10 +10,10 @@ var mouse = {
     clicked: false
 };
 var dictKeys = {
-    ArrowLeft: [0, -1],
-    ArrowRight: [0, 1],
-    s: [1, -1],
-    d: [1, 1]
+    37: [0, -1], //Left Arrow
+    39: [0, 1], //Right Arrow
+    68: [1, 1], //d
+    83: [1, -1] //s
 };
 
 document.addEventListener("click", function () {
@@ -29,27 +29,30 @@ function doMouseMove(e) {
     mouse.x = e.pageX - Game.canvas.offsetLeft;
     mouse.y = e.pageY;
 }
+
+// Using "keyCode" instead of "Key" for Chrome support. Not ideal since it's deprecate. To improve in the future.
+
 function keyDownHandler(e, balls) {
     "use strict";
-    if (e.key in dictKeys) {
-        var moveD = dictKeys[e.key];
+    if (e.keyCode in dictKeys) {
+        var moveD = dictKeys[e.keyCode];
         if (moveD[1] == 1) {
             balls[moveD[0]].rightPressed = true;
         }
         else {
-            balls[dictKeys[e.key][0]].leftPressed = true;
+            balls[dictKeys[e.keyCode][0]].leftPressed = true;
         }
     }
 }
 function keyUpHandler(e, balls) {
     "use strict";
-    if (e.key in dictKeys) {
-        var moveD = dictKeys[e.key];
+    if (e.keyCode in dictKeys) {
+        var moveD = dictKeys[e.keyCode];
         if (moveD[1] == 1) {
             balls[moveD[0]].rightPressed = false;
         }
         else {
-            balls[dictKeys[e.key][0]].leftPressed = false;
+            balls[dictKeys[e.keyCode][0]].leftPressed = false;
         }
     }
 }
@@ -145,14 +148,11 @@ function drawMenu(name) {
 
 }
 function updateButton(butt) {
-    console.log(mouse.clicked);
     if (mouse.x >= butt.rX && mouse.x <= butt.rX + butt.W && mouse.y >= butt.rY && mouse.y <= butt.rY + butt.H && mouse.clicked == false) {
         butt.color = butt.hoverColor;
-        console.log("hovering button " + butt.text)
     }
     else if (mouse.x >= butt.rX && mouse.x <= butt.rX + butt.W && mouse.y >= butt.rY && mouse.y <= butt.rY + butt.H && mouse.clicked == true) {
         butt.action();
-        console.log("clicked button " + butt.text);
     }
     else {
         butt.color = butt.normalColor;
@@ -169,21 +169,25 @@ function drawButton(butt) {
 }
 
 
-function touchWalls() { //TODO
+function touchWalls(ball) {
     "use strict";
-    return (balls[1].X <= balls[1].radius || balls[1].X >= Game.canvas.width - balls[1].radius || balls[1].Y <= balls[1].radius || balls[1].Y >= Game.canvas.height - balls[1].radius);
+    return (ball.x <= ball.r ||
+            ball.x >= Game.canvas.width - ball.r ||
+            ball.y <= ball.r ||
+            ball.y >= Game.canvas.height - ball.r);
 }
 
 function distance(x1, x2, y1, y2) {  // Compute distance between 2 points
     "use strict";
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
-function touchTrail() {      //detect contact with trail
+
+function touchTrail(ball,balls) {      //detect contact with trail
     "use strict";
     var t, dist;
     for (t = 0; t < balls[1].trail.length; t += 1) {
         dist = distance(balls[1].trail[t].X, balls[1].X, balls[1].trail[t].Y, balls[1].Y);
-        if (dist <= balls[1].radius * 2) {
+        if (dist <= balls[1].r * 2) {
             console.log(dist);
             return true;
 
@@ -244,13 +248,25 @@ function drawBall(ball) {
     "use strict";
     Game.ctx.beginPath();
     Game.ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2, true);
-    Game.ctx.fillStyle = ball.color;
+    if(ball.hole==false){
+        Game.ctx.fillStyle = ball.color;
+    }
+    else{
+        Game.ctx.fillStyle = "rgba(122,122,122,0.1)";
+
+    }
     Game.ctx.fill();
     Game.ctx.closePath();
 }
 
 function updateBall(ball) {
     "use strict";
+    if(ball.framesToHole>0){
+        ball.framesToHole--;
+    }
+    else{
+        ball.hole=true;
+    }
     if (ball.rightPressed) {
         ball.dir += Math.PI * ball.speed / 100;
     }
@@ -265,23 +281,35 @@ function updateBall(ball) {
     dx = dx * ball.speed / dxdy;
     dy = dy * ball.speed / dxdy;
 
+    if(touchWalls(ball)){
+        ball.touchesWall=true;
+    }
+
     ball.oldX = ball.x;
     ball.oldY = ball.y;
 
     ball.x += dx;
     ball.y += dy;
-    if (ball.hole = false) {
+    if (ball.hole == false) {
         ball.tempTrail.push({  //if it's not in hole state, add current position to temporary trail
             X: ball.x,
             Y: ball.y,
-            radius: ball.radius
+            r: ball.r
         });
         ball.counter++;
-        if (ball.counter > (ball.radius + 1)) {
-            for (var i = 0; i < tempTrail.length; i++) {
-                ball.trail.push(tempTrail[i]);
+        if (ball.counter > (ball.r + 1)) {
+            for (var i = 0; i < ball.tempTrail.length; i++) {
+                ball.trail.push(ball.tempTrail[i]);
             }
             ball.tempTrail = [];
+        }
+    }
+    else{
+        ball.holeStart ++;
+        if(ball.holeStart==15){
+            ball.hole=false;
+            ball.framesToHole=Math.floor(Math.random()*100 + 15);
+            ball.holeStart=0;
         }
     }
     return ball;
@@ -298,6 +326,11 @@ function updateAndDraw(balls) {
         drawBall(balls[i]);
     }
 
+    for (i = 0; i < balls.length; i += 1) {
+        if(balls[i].touchesWall){
+            alert("You lost!");
+        }
+    }
     window.requestAnimationFrame(function () {
         updateAndDraw(balls);
     });
